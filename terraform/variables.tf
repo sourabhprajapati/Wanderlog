1b8e8de9ed3aacd260bd3c92497c0d72ebea28df
sourabh <sourabhprajapati920@gmail.com>
2026-06-07 16:08:00 +0530

    sdvcfbvgnhb

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "eu-north-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-05d62b9bc5a6ca605"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = "c7i-flex.large"
 }
